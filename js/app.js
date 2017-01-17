ca959244bcd07ed975e718c6eb0d664b703eace5
Filiberto Reyes <filibertoreyes55@gmail.com>
2017-01-17 12:56:30 -0800

    Moved script over to app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,55 @@
-var op1 = 10;
-var op2 = 20;
 
+var output = document.Calculator.display.value;
+var cleared = true;
+var evaluated = false;
 
-function int changeVar(input) {
-  var op1 = document.getElementById("para");
-  console.log(op1);
+function numBtn(a) {
+
+	if(evaluated) {
+		btnClear();
+		console.log('evaluated is true')
+	}
+
+	if(('' === output) && (a == '0') && cleared) {
+		console.log('Display is clear!')
+		return;
+	}
+
+	else {
+		evaluated = false;
+		cleared = false;
+		document.Calculator.display.value += a;
+	}
 }
+
+function division () {
+	evaluated = false;
+	document.Calculator.display.value +='/';
+}
+
+function multiplication () {
+	evaluated = false;
+	document.Calculator.display.value +='*';
+}
+
+function subtraction () {
+	evaluated = false;
+	document.Calculator.display.value +='-';
+}
+
+function addition () {
+	evaluated = false;
+	document.Calculator.display.value +='+';
+}
+
+function equals () {
+	evaluated = true;
+	document.Calculator.display.value = eval(document.Calculator.display.value);
+	console.log('answer is: ' + document.Calculator.display.value);
+}
+
+function btnClear () {
+	cleared = true;
+	document.Calculator.display.value = "";
+}
+
